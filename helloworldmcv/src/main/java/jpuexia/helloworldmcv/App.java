8d1c818d850cd212b73357d8230c20dc892e9f7a
Asus <Asus@Asus-PC>
2016-06-07 09:40:11 +0200

    Worshop Java

---
+++ helloworldmcv/src/main/java/jpuexia/helloworldmcv/App.java
@@ -0,0 +1,13 @@
+package jpuexia.helloworldmcv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
